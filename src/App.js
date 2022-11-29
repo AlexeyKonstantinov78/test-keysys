facc13a332195054bd8d208d02cdf54bd46c422e
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-11-29 14:03:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import style from './App.module.css';
 import { Container } from './components/Container/Container';
 import { Correspondent } from './components/Correspondent/Correspondent';
@@ -6,6 +7,56 @@ import { GridList } from './components/Gridlist/GridList';
 import { HeaderNumberDate }
   from './components/HeaderNumberDate/HeaderNumberDate';
 import { SendersAccount } from './components/SendersAccount/SendersAccount';
+
+const listAccount = [
+  {
+    numberccor: '2221335409090932',
+    name: 'MasterCard',
+  },
+  {
+    numberccor: '4710216477552728',
+    name: 'Visa',
+  },
+  {
+    numberccor: 'Discover Card',
+    name: '6011383809641644',
+  },
+];
+
+const arr = [
+  {
+    number: '', // номер
+    date: '2021-10-01', // дата
+    date_exec: '2021-10-01', // дата проводки
+    caccount1: '', // Счет отправителя
+    corr1_n: '', // наименование отправителя
+    corr2_n: '', //
+    inc_n: '', //
+    items: [
+      {
+        id: '1', //
+        summ: '', //
+        rcaccount: '', //
+        dt: '', //
+        kt: '', //
+      },
+      {
+        id: '2', //
+        summ: '', //
+        rcaccount: '', //
+        dt: '', //
+        kt: '', //
+      },
+      {
+        id: '3', //
+        summ: '', //
+        rcaccount: '', //
+        dt: '', //
+        kt: '', //
+      },
+    ], // список
+  },
+];
 
 function App() {
   return (
